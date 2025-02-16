1ffe382e9abc18d4e335f3745a6e863b993c897c
Eric van Riet Paap <eric@Erics-Mac-mini.home>
2025-02-16 20:45:59 +0100

    Computer upcoming energy prices every 15 minutes by polling from Tibber API

--- battery-autopilot/server/server.ts
+++ battery-autopilot/server/server.ts
@@ -1,23 +1,22 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { TibberQuery, IConfig } from "tibber-api";
 
 // There is a rate limit of 100 requests in 5 minutes per IP address intended to protect the API.
 // So don't poll more often then once every 3 seconds
 
-import { TibberQuery, IConfig } from "tibber-api";
-
 const SECOND_MS = 1000;
 const MINUTE_MS = 60 * SECOND_MS;
-const UPDATE_INTERVAL = 15 * MINUTE_MS;
+const HOUR_MS = 60 * MINUTE_MS;
 
-// console.log(import.meta.env.VITE_TIBBER_APIKEY);
+const UPDATE_INTERVAL = 15 * MINUTE_MS;
 
 // Config object needed when instantiating TibberQuery
 const config: IConfig = {
   active: true,
   apiEndpoint: {
-    apiKey: import.meta.env.VITE_TIBBER_APIKEY,
+    apiKey: process.env.VITE_TIBBER_APIKEY,
     queryUrl: "https://api.tibber.com/v1-beta/gql",
   },
 };
@@ -28,36 +27,59 @@ let autopilotState = {
   enabled: false,
 };
 
-const pollTibber = () => {
-  console.log("TODO: pollTibber");
+const pollTibber = async () => {
+  const homeId = (await tibberQuery.getHomes())[0].id;
+  // console.table(`homeId: ${homeId}`);
+
+  // const currentEnergyPrice = await tibberQuery.getCurrentEnergyPrice(homeId);
+  // console.table(currentEnergyPrice);
+
+  const todaysEnergyPrices = await tibberQuery.getTodaysEnergyPrices(homeId);
+  // console.table(todaysEnergyPrices);
+
+  const tomorrowsEnergyPrices = await tibberQuery.getTomorrowsEnergyPrices(
+    homeId
+  );
+  // console.table(tomorrowsEnergyPrices);
+
+  const prices = [...todaysEnergyPrices, ...tomorrowsEnergyPrices];
+  const upcomingPrices = prices.filter(
+    (price) =>
+      new Date(price.startsAt).getTime() >= new Date().getTime() - 1 * HOUR_MS
+  );
+  console.table(upcomingPrices);
 };
-pollTibber();
-setInterval(pollTibber, UPDATE_INTERVAL);
+if (!process.env.VITE_TIBBER_APIKEY) {
+  console.error("error: VITE_TIBBER_APIKEY environment variable not found");
+} else {
+  pollTibber();
+  setInterval(pollTibber, UPDATE_INTERVAL);
+}
 
 //
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use((req, _res, next) => {
-  console.log(`${req.method} request for '${req.url}'`);
-  next(); // Pass control to the next middleware or route handler
-});
+// app.use((req, _res, next) => {
+//   console.log(`${req.method} request for '${req.url}'`);
+//   next(); // Pass control to the next middleware or route handler
+// });
 
 // Endpoint to get autopilot status
 app.get("/api/autopilot/v1/status", (req, res) => {
-  console.log("Autopilot status requested");
+  // console.log("Autopilot status requested");
   res.json(autopilotState);
 });
 
 app.post("/api/autopilot/v1/enable/:enabled", (req, res) => {
   const enabled = req.params.enabled === "true";
-  console.log(`Autopilot enabled ${enabled} requested`);
+  console.log(`Autopilot enable ${enabled} requested`);
   autopilotState.enabled = enabled;
   res.json(autopilotState);
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = Bun.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`battery-autopilot server running on http://localhost:${PORT}`);
 });
